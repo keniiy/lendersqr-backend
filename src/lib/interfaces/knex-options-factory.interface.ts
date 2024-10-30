fec4e632013db5fff679ccca0a1039b230699208
keniiy <kehindekehinde894@gmail.com>
2024-10-30 14:19:07 +0100

    Chores: Knex set up

---
+++ src/lib/interfaces/knex-options-factory.interface.ts
@@ -0,0 +1,7 @@
+import { KnexModuleOptions } from './knex-options.interface';
+
+export interface KnexOptionsFactory {
+  createKnexOptions(
+    connectionName?: string,
+  ): Promise<KnexModuleOptions> | KnexModuleOptions;
+}
